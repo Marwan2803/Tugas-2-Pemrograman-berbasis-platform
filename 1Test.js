294cb50db2bc0edc371973bb8ecd4b402c4ce2be
Marwan2803 <marwanmuharam207@gmail.com>
2024-10-04 10:10:17 +0700

    Add files via upload

---
+++ 1Test.js
@@ -0,0 +1,11 @@
+// Nilai pi
+var pi = 3.14159;
+
+// Jari-jari lingkaran
+var jariJari = 10; 
+
+// Rumus keliling lingkaran
+var keliling = 2 * pi * jariJari;
+
+// Menampilkan hasil keliling lingkaran
+console.log("Keliling lingkaran: " + keliling);
